fb00367e34c6bb64585cd18015d37df93e911452
uchiyama-shintaro <uchiyama-shintaro@9115-080616.ad.imjp.co.jp>
2017-06-20 20:00:32 +0900

    add regist routing

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -24,29 +24,13 @@ return array(
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
             // using the path /application/:controller/:action
-            'application' => array(
-                'type'    => 'Literal',
+            'regist' => array(
+                'type'    => 'Segment',
                 'options' => array(
-                    'route'    => '/application',
+                    'route'    => '/regist[/:action[/:cvid]][/]',
                     'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
+                        'controller'    => 'Application\Controller\Regist',
+                        'action'        => 'input',
                     ),
                 ),
             ),
@@ -73,7 +57,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\Regist' => 'Application\Controller\RegistController'
         ),
     ),
     'view_manager' => array(
---
+++ module/Application/src/Application/Controller/RegistController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class RegistController extends AbstractActionController
+{
+     /**
+     * 入力画面表示
+     * @return ViewModel
+     */
+    public function inputAction()
+    {
+        return new ViewModel();
+    }
+
+     /**
+     * 確認画面表示
+     * @return ViewModel
+     */
+    public function confirmAction()
+    {
+        return new ViewModel();
+    }
+
+     /**
+     * 仮登録画面表示
+     * @return ViewModel
+     */
+    public function provCompleteAction()
+    {
+        return new ViewModel();
+    }
+
+     /**
+     * メール確認画面表示
+     * @return ViewModel
+     */
+    public function checkEmailAction()
+    {
+        return new ViewModel();
+    }
+}
